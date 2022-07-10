30c82b8e10b1988c3000d4979bf2b476d06780dc
GeonDBD <51849827+GeonDBD@users.noreply.github.com>
2022-07-10 22:04:50 +0800

    Added ObjectiveActor
    添加了ObjectiveActor
    添加了拾取的动画和HUD提示

---
+++ Source/FPSGame/Private/BlackHole.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlackHole.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+ABlackHole::ABlackHole()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	RootComponent = MeshComp;
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetupAttachment(MeshComp);
+}
+
+// Called when the game starts or when spawned
+void ABlackHole::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABlackHole::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/FPSGame/Private/FPSObjectiveActor.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSObjectiveActor.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "FPSCharacter.h"
+
+// Sets default values
+AFPSObjectiveActor::AFPSObjectiveActor()
+{
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	// 设置碰撞为无碰撞
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = MeshComp;
+
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	// 设置碰撞为只查询
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	// 为所有通道设置碰撞响应设置为：忽略
+	SphereComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	// 为一个通道设置碰撞响应设置为：给pawn，重叠
+	SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	SphereComp->SetupAttachment(MeshComp);
+}
+
+// Called when the game starts or when spawned
+void AFPSObjectiveActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// 调用效果函数
+	PlayEffects();
+}
+
+// 游戏效果
+void AFPSObjectiveActor::PlayEffects()
+{
+	UGameplayStatics::SpawnEmitterAtLocation(this, PickupFX, GetActorLocation());
+}
+
+// 重载overlap函数
+void AFPSObjectiveActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	// 调用效果函数
+	PlayEffects();
+
+	// 定义我的角色，并强制装换
+	AFPSCharacter* MyCharacter = Cast<AFPSCharacter>(OtherActor);
+	if (MyCharacter)
+	{
+		MyCharacter->bIsCarryingObjective = true;
+
+		Destroy();
+	}
+}
